4736de2edb25adbc42a64171611d04b6a89e3b05
bhavya kalariya <bhavyapatel8980@gmail.com>
2026-01-21 00:07:04 +0530

    Initial commit - QueryWhisperer Text-to-SQL chatbot

---
+++ README.md
@@ -0,0 +1,23 @@
+# Introducing Vanna AI
+Text-To-SQL Playground
+
+This GitHub repository is a companion resource to the Medium article [Text-to-SQL Just Got Easier: Meet Vanna AI, Your Text-to-SQL Assistant
+](https://medium.com/mitb-for-all/text-to-sql-just-got-easier-meet-vanna-ai-your-rag-powered-sql-sidekick-e781c3ffb2c5)
+
+[![Watch the video](https://raw.githubusercontent.com/tituslhy/glowing-engine/main/images/bullrun_thumbnail.png)](https://raw.githubusercontent.com/tituslhy/glowing-engine/main/media/bullrun_app.mp4)
+
+## Setup
+This repository uses the [uv package installer](https://docs.astral.sh/uv/pip/packages/). 
+
+To create a virtual environment with the dependencies installed, simply type in your terminal:
+```
+uv sync
+```
+
+You'll need to ingest the data into an SQLite database before you can interact with the app. Simply run the codes in `./notebooks/setup_db.ipynb` to ingest the ticker information from [Yahoo Finance](https://github.com/ranaroussi/yfinance).
+
+## To run Chainlit app
+This spins up your application on port 8000
+```
+chainlit run app.py
+```
---
+++ vn.py
@@ -0,0 +1,84 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+import warnings
+from typing import Dict, Optional, Annotated, Tuple, List
+import yaml
+
+from vanna.ollama import Ollama
+from vanna.chromadb.chromadb_vector import ChromaDB_VectorStore
+from tqdm import tqdm
+
+_ = load_dotenv(find_dotenv())
+warnings.filterwarnings("ignore")
+
+__curdir__ = os.getcwd()
+if "notebooks" in __curdir__:
+    chroma_path = "../chroma"
+    yaml_file_path = "../config/training.yaml"
+else:
+    chroma_path = "./chroma"
+    yaml_file_path = "./config/training.yaml"
+
+class MyVanna(ChromaDB_VectorStore, Ollama):
+    """Our main Vanna handler. Inherits from Vanna's ChromaDB_VectorStore
+    and Ollama which implement many of the abstract methods in Vanna's
+    Base class. The only thing we need to do is define the constructor where
+    we initialize the different objects."""
+    
+    def __init__(
+        self, 
+        config: Optional[Annotated[
+            Dict[str, str], 
+            """The configuration for the vector store or the LLM you want to use. 
+            For e.g. {'model':'llama3'}"""
+        ]]
+    ) -> None:
+        ChromaDB_VectorStore.__init__(self, config=config)
+        Ollama.__init__(self, config=config)
+
+def load_query_data(yaml_file_path: str) -> List[Tuple[str, str]]:
+    """Returns a list of training queries for LLM training.
+    
+    This is a mission critical function.
+    """
+    try:
+        with open(yaml_file_path, 'r') as file:
+            try:
+                documents = yaml.safe_load(file)
+                if not isinstance(documents, list):
+                    raise ValueError("YAML content is not a list of documents.")
+            except yaml.YAMLError as yaml_error:
+                raise ValueError(f"Error parsing YAML file: {yaml_error}") from yaml_error
+    except FileNotFoundError as fnf_error:
+        raise FileNotFoundError(f"Error: The file at {yaml_file_path} was not found.") from fnf_error
+    except Exception as e:
+        raise Exception(f"An unexpected error occurred: {e}") from e
+
+    return [(doc.get('question'), doc.get('answer')) for doc in documents]
+    
+print("Instantiating Vanna...")
+vn = MyVanna(config={
+    "model": "llama3",  # Change to your preferred Ollama model (e.g., mistral, codellama, etc.)
+    "path": chroma_path, #this is the specific key that Vanna looks for (reference: Vanna's ChromaDB_VectorStore source code)
+})
+
+print("Connecting Vanna to SQL database...")
+vn.connect_to_sqlite("./database/stocks.db")
+
+print("Training Vanna...")
+df_ddl = vn.run_sql("SELECT type, sql FROM sqlite_master WHERE sql is not null")
+for ddl in df_ddl['sql'].to_list():
+    vn.train(ddl=ddl)
+vn.train(documentation="Illumina is defined as ilmn")
+vn.train(documentation="Apple is defined as aapl")
+vn.train(documentation="NVIDIA is defined as nvda")
+
+queries = load_query_data(yaml_file_path=yaml_file_path)
+for question, sql in tqdm(queries):
+    vn.train(question=question, sql=sql)
+
+if __name__ == "__main__":
+    from vanna.flask import VannaFlaskApp
+    
+    app = VannaFlaskApp(vn)
+    app.run()
